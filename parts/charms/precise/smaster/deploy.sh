3bddf655c46d98360db7366fc9cf6a7699486f20
prosset <paulrosset96@gmail.com>
2018-07-01 10:33:50 +0000

    Fix hooks with correct data structure, add script for deployment summary

--- parts/charms/precise/smaster/deploy.sh
+++ parts/charms/precise/smaster/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+juju deploy cs:vault-1
 juju deploy ./
 juju add-relation kafka smaster
+juju add-relation kubernetes-master smaster
+juju add-relation vault smaster
 juju expose smaster
