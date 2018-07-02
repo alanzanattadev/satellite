9d58b875ae164bd73d2a9f3edf29484373c5c2c1
prosset <paulrosset96@gmail.com>
2018-07-02 16:39:29 +0000

    Add neo4j

--- parts/charms/smaster/deploy.sh
+++ parts/charms/smaster/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 juju deploy cs:vault-1
+juju deploy ./../layers/neo4j/
 juju deploy ./
+juju add-relation neo4j smaster
 juju add-relation kafka smaster
 juju add-relation kubernetes-master smaster
 juju add-relation vault smaster
