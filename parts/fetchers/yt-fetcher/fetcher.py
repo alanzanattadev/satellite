59a16b66398a01acde16841f61f4649c88c8b99d
Paul Rosset <paulrosset96@gmail.com>
2018-07-09 15:47:59 +0100

    Add generic fetcher for subcriptions && playlists

--- parts/fetchers/yt-fetcher/fetcher.py
+++ parts/fetchers/yt-fetcher/fetcher.py
@@ -3,12 +3,19 @@
 import requests
 import json
 import pymongo
+from utils import insertOne
 
 
-class YTFetcher:
+class YTFetcher():
     def __init__(self, apiKey, ytUsername):
         self.apiKey = apiKey
         self.ytUsername = ytUsername
+        self.channelID = None
+        self.client = pymongo.MongoClient(
+            "localhost", 27017)  # Mettre variable d'env
+        self.db = self.client["test-ytFetcher"]
+        self.subscription = self.db["subscriptions"]
+        self.playlists = self.db["playlists"]
 
     def getChannelID(self):
         try:
@@ -16,38 +23,49 @@ class YTFetcher:
                        "forUsername": self.ytUsername, "part": "id"}
             r = requests.get(
                 "https://www.googleapis.com/youtube/v3/channels", params=payload)
-            if r.status_code == 200:
-                self.channelID = json.loads(r.content)["items"][0]["id"]
+            data = json.loads(r.content)
+            if r.status_code == 200 and len(data["items"]) != 0:
+                self.channelID = data["items"][0]["id"]
                 print "ChannelID of " + self.ytUsername + ": " + self.channelID
             else:
-                raise Exception("Error while fetching channelID")
+                raise Exception(
+                    "Error while fetching channelID, you may not need to get your channelID")
         except Exception as err:
             print err
 
-    def subToChannel(self, maxResultsPerPage=10):
+    def fetcher(self, typeFetching, collectionDep, maxResultsPerPage=10):
         try:
             isNextPage = True
             pageNb = 0
-            payload = {"key": self.apiKey, "channelId": self.channelID,
+            itemsNb = 0
+            payload = {"key": self.apiKey, "channelId": self.channelID if self.channelID is not None else self.ytUsername,
                        "part": "snippet", "maxResults": maxResultsPerPage}
             while isNextPage:
                 r = requests.get(
-                    "https://www.googleapis.com/youtube/v3/subscriptions", params=payload)
+                    "https://www.googleapis.com/youtube/v3/" + typeFetching, params=payload)
+                data = json.loads(r.content)
                 if r.status_code == 200:
                     pageNb += 1
-                    if "nextPageToken" in json.loads(r.content):
-                        nextPageToken = json.loads(r.content)["nextPageToken"]
+                    if "nextPageToken" in data:
+                        nextPageToken = data["nextPageToken"]
                         payload["pageToken"] = nextPageToken
                     else:
                         isNextPage = False
-                    print "Fetched subscriptions page: " + str(pageNb)
+                    for item in data["items"]:
+                        insertOne(item["snippet"], collectionDep)
+                        itemsNb += 1
+                        print "Entry added " + str(itemsNb)
+                    print "Fetched page: " + str(pageNb)
                 else:
                     raise Exception(
-                        "Error while fetching subscriptions, do not forget to call getChannelId() before!")
+                        "Error while fetching, if you have a custom channelId make sure to call getChannelId() method!")
         except Exception as err:
             print err
 
+    def fetcherPlaylistMusic(self, collectionDep, maxResultsPerPage=10):
 
-fetch = YTFetcher("AIzaSyCvJT0slEo5IYSbeEd1UeJJVw9vO3vlMLc", "XTRSilent")
+
+fetch = YTFetcher("AIzaSyCvJT0slEo5IYSbeEd1UeJJVw9vO3vlMLc",
+                  "XTRSilent")
 fetch.getChannelID()
-fetch.subToChannel()
+fetch.fetcher("playlists", fetch.playlists, 5)
