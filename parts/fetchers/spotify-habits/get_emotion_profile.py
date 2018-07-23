ea33392c62c4f39473629dffa278508b931f1766
alanzanattadev <alan.zanatta@outlook.com>
2018-07-23 16:04:44 +0100

    Adds averages of audio features

--- parts/fetchers/spotify-habits/get_emotion_profile.py
+++ parts/fetchers/spotify-habits/get_emotion_profile.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     emotions_collection = db[mongo_emotions_collection_name]
     mongo_audio_features_collection_name = os.environ.get(
         'MONGO_AUDIO_FEATURES_COLLECTION',
-        'audio_features'
+        'audio-features'
     )
     audio_features_collection = db[mongo_audio_features_collection_name]
 
@@ -40,5 +40,31 @@ if __name__ == '__main__':
 
     emotions = [anger, surprise, fear, disgust, joy, sadness]
     emotions.sort(key=lambda a: a['count'], reverse=True)
+    print("----------------\nLyrics Emotions\n----------------")
     for emotion in emotions:
         print("%s: %d" % (emotion['type'].title(), emotion['count']))
+
+    print("----------------\nAudio Features\n----------------")
+    cursor = audio_features_collection.aggregate([
+        {
+            '$match': {
+                'user_id': username,
+            }
+        },
+        {
+            '$group': {
+                '_id': "$user_id",
+                'danceabilityAvg': {'$avg': "$audio_features.danceability"},
+                'energyAvg': {'$avg': '$audio_features.energy'},
+                'valenceAvg': {'$avg': '$audio_features.valence'},
+                'instrumentalnessAvg': {'$avg': '$audio_features.instrumentalness'},
+                'speechinessAvg': {'$avg': '$audio_features.speechiness'}
+            }
+        }
+    ])
+    for result in cursor:
+        print('Danceability: %f' % (result['danceabilityAvg']))
+        print('Energy: %f' % (result['energyAvg']))
+        print('Valence: %f' % (result['valenceAvg']))
+        print('Instrumentalness: %f' % (result['instrumentalnessAvg']))
+        print('Speechiness: %f' % (result['speechinessAvg']))
