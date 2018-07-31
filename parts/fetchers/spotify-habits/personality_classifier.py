7e01458667e6cbc6a96368b48034e0e19c82caf8
alanzanattadev <alan.zanatta@outlook.com>
2018-07-31 12:45:04 +0100

    Refactors classifier into modular code

--- parts/fetchers/spotify-habits/personality_classifier.py
+++ parts/fetchers/spotify-habits/personality_classifier.py
@@ -1,33 +1,9 @@
 #!/usr/bin/env python3
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np
 import satellipy.utils.cli as CliUtils
-import satellipy.analysis as Analysis
 import satellipy.configuration.mongo as MongoConf
-import satellipy.personalities
+import satellipy.ai.model as Model
+import satellipy.ai.predict as Predictor
 
-
-def get_training_set(
-    emotions_collection, audio_features_collection, personalities_collection
-):
-    train_accounts = []
-    train_labels = []
-    cursor = personalities_collection.find({'processed': True, 'predicted': False})
-    for doc in cursor:
-        train_accounts.append(
-            Analysis.get_features_for_user(
-                doc['user_id'],
-                emotions_collection,
-                audio_features_collection
-            )
-        )
-        train_labels.append(
-            satellipy.personalities.personalities.index(
-                doc['personality_label']
-            )
-        )
-    return (np.array(train_accounts), np.array(train_labels))
 
 
 if __name__ == "__main__":
@@ -37,39 +13,5 @@ if __name__ == "__main__":
     audio_features_collection = mongo_client['collections']['audio_features']
     personalities_collection = mongo_client['collections']['personalities']
 
-    model = keras.Sequential([
-        keras.layers.Dense(128, activation=tf.nn.relu),
-        keras.layers.Dense(16, activation=tf.nn.softmax)
-    ])
-
-    model.compile(
-        optimizer=tf.train.AdamOptimizer(),
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-
-    train_accounts, train_labels = get_training_set(
-        emotions_collection,
-        audio_features_collection,
-        personalities_collection
-    )
-    model.fit(train_accounts, train_labels, epochs=10000)
-
-    test_accounts, test_labels = (train_accounts, train_labels)
-    test_loss, test_acc = model.evaluate(test_accounts, test_labels)
-    print('Test accuracy:', test_acc)
-
-    target_accounts = np.array([
-        Analysis.get_features_for_user(
-            username, emotions_collection, audio_features_collection
-        )
-    ])
-    predictions = model.predict(target_accounts)
-    results = [{'probability': p, 'type': satellipy.personalities.personalities[i]} for i,p in enumerate(predictions[0])]
-    results.sort(key=lambda x: x['probability'], reverse=True)
-    print("-------- Results --------")
-    print("Personality: %s" % (satellipy.personalities.personalities[np.argmax(predictions[0])]))
-    print("----------")
-    for result in results:
-        print("%s: %f" % (result['type'], result['probability']))
-    print("-------------------------")
+    model = Model.get_trained_model(emotions_collection, audio_features_collection, personalities_collection)
+    Predictor.predict_for_user(username, model, emotions_collection, audio_features_collection)
