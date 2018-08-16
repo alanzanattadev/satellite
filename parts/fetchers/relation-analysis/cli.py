93d7cef73111cb5dbf2f7f9d1f342722ac1754a7
Paul Rosset <paulrosset96@gmail.com>
2018-08-16 17:45:47 +0100

    Relation analysis (#17)
    * Init TwitterAnalysis
    
    * setUpDb func and getData from mongodb
    
    * Add docker env for script
    
    Fix docker file
    
    * Add analysis on each tweet
    
    * Add relations profile related to each tweet of an user
    
    * Add sentiment profile
    
    * Add doc
    
    * v1 of an analysis based on a set of tweet, see documentation
    
    * Add dockerfile and add it on readme
    
    * Add cli + fix minor change on TwitterAnalysis
    
    * Minor refactoring
    
    * Add first tweet evocation on relations
    
    * Init neo4j
    
    * Doccc
    
    * Integrate GraphDB neo4j
    
    * Add Docker for twitter-relations and doc related to it
    
    * Integrate GraphDB class for neo4j in the CLI interface
    
    * run multiple RUN command in Dockerfile
    
    * Adjust order of RUN command on dockerfile

---
+++ parts/fetchers/relation-analysis/cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import argparse
+from main import TwitterAnalysis
+from graph import GraphDB
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("twitterUser", type=str, help="Twitter ID")
+    parser.add_argument("-f", "--filter", type=str,
+                        help="Filter the processed tweet", default={})
+    parser.add_argument("-p", "--process", action="store_true",
+                        help="Draw a first analysis for each tweet, then insert in database (dbDest)")
+    parser.add_argument("-g", "--graph", action="store_true",
+                        help="Insert in a Neo4j graph Database the relation for twitter")
+    args = parser.parse_args()
+    analysis = TwitterAnalysis(
+        args.twitterUser, args.filter if args.filter else {})
+    if args.process:
+        analysis.procOnEachTweet()
+    dataProfile = analysis.mapReduceOnEachTweet()
+    if args.graph and dataProfile:
+        graph = GraphDB(dataProfile)
+        graph.fetchNodesRelatedProfile()
+
+
+if __name__ == "__main__":
+    main()
