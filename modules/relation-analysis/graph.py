97e629f6fa19fa9356daa9eec302fc606a18dfbb
Paul Rosset <paulrosset96@gmail.com>
2018-08-19 16:20:19 +0100

    add encode ascii

--- modules/relation-analysis/graph.py
+++ modules/relation-analysis/graph.py
@@ -13,7 +13,8 @@ class GraphDB:
         self.driver = GraphDatabase.driver(
             self.uri, auth=(self.user, self.password))
         self.profile = profileAfterProcess
-        self.owner = profileAfterProcess["profileUser"]
+        self.owner = profileAfterProcess["profileUser"].encode(
+            "ascii", "ignore").decode()
         self.createOneNodeRelation(
             self.owner, "Log: Creation of mother User...")
         self.fetchNodesLanguage()
@@ -21,24 +22,25 @@ class GraphDB:
     def createOneNodeRelation(self, name, msglog):
         with self.driver.session() as session:
             print(msglog)
-            return session.run("CREATE (a:Person {name: $name})", name=name)
+            return session.run("CREATE (a:Person {name: $name})", name=name.encode("ascii", "ignore").decode())
 
     def createRelationUser(self, user, data):
         with self.driver.session() as session:
-            print("Log: Creation of User relation " + user)
-            return session.run("MATCH (a:Person) WHERE a.name = $nameA CREATE (b:Person {name: $nameB})-[r:RELATION {interactions: $interac, first_interaction: $firstI}]->(a)", nameA=self.owner, nameB=user, interac=data["count"], firstI=data["first_interac"].strftime("%d/%m/%Y"))
+            print("Log: Creation of User relation " +
+                  user.encode("ascii", "ignore").decode())
+            return session.run("MATCH (a:Person) WHERE a.name = $nameA CREATE (b:Person {name: $nameB})-[r:RELATION {interactions: $interac, first_interaction: $firstI}]->(a)", nameA=self.owner, nameB=user.encode("ascii", "ignore").decode(), interac=data["count"], firstI=data["first_interac"].strftime("%d/%m/%Y"))
 
     def createNodeLang(self, lang, data):
         with self.driver.session() as session:
             print("Log: Creation of Lang node " + lang)
-            return session.run("CREATE (a:Language {name: $lang, used: $data})", lang=lang, data=data)
+            return session.run("CREATE (a:Language {name: $lang, used: $data})", lang=lang.encode("ascii", "ignore").decode(), data=data)
 
     def createRelationLangToUsers(self, user, langUsed):
         with self.driver.session() as session:
             print("Log: Creation of RelationShip between Lang and User")
             for lang in langUsed:
                 session.run(
-                    "MATCH (a:Person),(b:Language) WHERE a.name = $user AND b.name = $lang CREATE (a)-[r:RELANG]->(b)", user=user, lang=lang)
+                    "MATCH (a:Person),(b:Language) WHERE a.name = $user AND b.name = $lang CREATE (a)-[r:RELANG]->(b)", user=user.encode("ascii", "ignore").decode(), lang=lang.encode("ascii", "ignore").decode())
 
     def fetchNodesRelatedProfile(self):
         for user, data in self.profile["relations"].items():
