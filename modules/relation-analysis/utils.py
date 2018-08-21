823664243bc33ba98c8ab9ea183bbacfc8d68ad8
Paul Rosset <paulrosset96@gmail.com>
2018-08-21 14:53:42 +0100

    add utils function fromUtf8ToAscii

--- modules/relation-analysis/utils.py
+++ modules/relation-analysis/utils.py
@@ -7,3 +7,7 @@ def fromInt64ToInt(o):
     if isinstance(o, numpy.int64):
         return int(o)
     raise TypeError
+
+
+def fromUtf8ToAscii(entry):
+    return entry.encode("ascii", "ignore").decode()
